16a0a5e0c1357e9de905d63e3891666f8d8b0519
Gimenez Martinez Matias <402016@sistemas.frc.utn.edu.ar>
2025-10-24 12:02:39 -0300

    Eliminada la variable de entorno BASE_PATH

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,6 @@
 const nextConfig = {
   output: 'export',
 
-  // Lee la variable de entorno BASE_PATH, si no existe, no usa nada.
-  basePath: process.env.BASE_PATH || '',
-
   images: {
     unoptimized: true,
   },
